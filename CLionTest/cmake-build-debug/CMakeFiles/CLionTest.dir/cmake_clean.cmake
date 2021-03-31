98d69f799455cd43946ce090b9c1645342d83ed1
YeongHoon Song <thddudgns972@gmail.com>
2021-03-31 15:58:55 +0900

    Add local files via Upload

---
+++ CLionTest/cmake-build-debug/CMakeFiles/CLionTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CLionTest"
+  "CLionTest.pdb"
+  "CMakeFiles/CLionTest.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLionTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
