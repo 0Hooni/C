4c90a06567ef3f26e34190ecfa058f1fd2e0dfee
YeongHoon Song <thddudgns972@gmail.com>
2021-10-09 15:31:53 +0900

    Add files via upload

--- Problem Solving/Week 5/W5_A.cpp
+++ Problem Solving/Week 5/W5_A.cpp
@@ -1,48 +1,53 @@
 #include <iostream>
-#include <string.h>
-#include <queue>
-#include <stack>
 #include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
+//global var area
+vector<int> num;
+vector<int> hz;
+//function area
 void init(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 }
 
-vector<int> hz;
-int num[5001];
-
 int main(){
     init();
     int t; cin >> t;
     while (t--){
+        hz.clear(); num.clear();
         int n; cin >> n;
 
-        for(int i=0; i<n; i++) cin >> num[i];
+        for(int i=0; i<n; i++) {
+            int tmp; cin >> tmp;
+            num.push_back(tmp);
+        }
 
-        int i=0;
-        while (num[i] != hz.front()) hz.push_back(num[i++]);
-
-        int hz_now = 0;
-        for(int j=i; j<n; j++){
-            if(num[j] == hz[hz_now]) {
-                hz_now++;
-                if(hz_now == hz.size()) hz_now = 0;
+        hz.push_back(num[0]); int x = 1;  //예상주기
+        for(int i=1; i<n; i++){
+            if(hz.front() != num[i]) {
+                hz.push_back(num[i]);
+                x++;
+            }
+            else break;
+        }
+        int count_same = 0;
+        for(int i=x; i<n; i++){
+            if(num[i] == num[i-x]) {
+                count_same++;
+                if(count_same == x) count_same = 0;
             }
             else {
-                while (i != j){
-                    hz.push_back(num[i++]);
-                }
-                hz_now = 0;
+                hz.push_back(num[x++]); i=x-1;
             }
         }
 
-        for(int j=0; j<hz.size(); j++)
-            cout << hz[j] << " ";
+        for(int i=0; i<hz.size(); i++)
+            cout << hz[i] << " ";
         cout << "\n";
     }
-
     return 0;
 }
