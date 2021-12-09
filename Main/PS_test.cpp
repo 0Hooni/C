ed3ecbd6ca5c9f12b1d313e993b215fb47906a81
YeongHoon Song <thddudgns972@gmail.com>
2021-12-09 22:33:03 +0900

    update data

--- Main/PS_test.cpp
+++ Main/PS_test.cpp
@@ -1,52 +1,107 @@
 #include <iostream>
-#include <vector>
-#include <string.h>
-#include <algorithm>
-#include <stack>
-#include <queue>
+#include <fstream>
+#include <string>
 using namespace std;
-void init(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-}
 
-int tri[101][101];
-
-int main(){
-    init();
-
-    int t; cin >> t;
-    while (t--){
-        memset(tri, 0, sizeof(tri));
-        int n; cin >> n;
-        for(int i=0;i < n; i++)
-            for(int j=0; j<=i; j++) {
-                int tmp; cin >> tmp;
-                tri[i][j] = tmp;
-                int _min = 0;
-
-                if(j == 0) _min = tri[i-1][j];
-                else if(j == i)  _min = tri[i-1][j-1];
-                else
-                    _min = (tri[i-1][j-1] < tri[i-1][j]) ? tri[i-1][j-1] : tri[i-1][j];
-
-                tri[i][j] += _min;
-            }
-
-        int res = tri[n-1][0];
-        for(int i=1; i<n; i++)
-            res = (tri[n-1][i] < res) ? tri[n-1][i] : res;
-
-        cout << res << "\n";
-
+class CommunityMember {
+private:
+    int index = 0;
+    string Name = "";
+    string RegistrationNumber = "";
+    bool Gender = true;
+    string PhoneNumber = "";
+    string EMail = "";
+public:
+    CommunityMember(){}
+    int getIndex() {
+        return index;
     }
+    void setIndex(int a) {
+        this->index = a;
+    }
+    void setName(string s) {
+        this->Name = s;
+    }
+    string getName() {
+        return Name;
+    }
+    ~CommunityMember(){}
+};
 
+
+class Employee : public CommunityMember {
+private:
+    string EmployeeNumber = "";
+    string LocalPhoneNumber = "";
+    string Rank = "";
+    string AccountNumber = "";
+public:
+    Employee(){}
+    ~Employee(){}
+};
+
+class Teacher : public Employee {
+    string Department = "";
+    string UniversitylssuanceDate = "";
+    string HomepageAddress = "";
+    string FinalEducation = "";
+public:
+    Teacher() {}
+    ~Teacher(){}
+};
+
+
+class Student : public CommunityMember {
+private:
+    string StudentID = "";
+    string Department = "";
+    int Grade = 0;
+    bool CurrentStudentPresence = true;
+public:
+    Student(){}
+    ~Student(){}
+};
+
+
+class Staff : public Employee {
+    string Responsibilities = "";
+public:
+    Staff(){}
+    ~Staff(){}
+};
+
+int main() {
+    ofstream os;
+    os.open("CommunityMember.info", ios::out | ios::binary);
+    Teacher osteacher;
+    for (int i = 1; i <= 20; i++) {
+       os.write(reinterpret_cast<const char*>(&osteacher), sizeof(Teacher));
+    }
+    os.close();
+
+    fstream is;
+    is.open("CommunityMember.info", ios::in | ios::out | ios::binary);
+
+    int a;
+    cin >> a;
+    string s;
+    cin >> s;
+    Teacher isteacher;
+    isteacher.setIndex(a);
+    isteacher.setName(s);
+    is.seekp((a - 1) * sizeof(Teacher));
+    is.write(reinterpret_cast<const char*>(&isteacher), sizeof(Teacher));
+
+    is.seekg(0);
+    for (int i = 0; i < 20; i++) {
+        is.read(reinterpret_cast<char*>(&isteacher), sizeof(Teacher));
+        cout << isteacher.getIndex() << ' ' << isteacher.getName() << '\n';
+    }
+    is.clear();
+    //cout << sizeof(CommunityMember) <<'\n'; //120
+    //cout << sizeof(Employee) << '\n'; //232
+    //cout << sizeof(Student) << '\n'; //184
+    //cout << sizeof(Teacher) << '\n'; //344
+    //cout << sizeof(Staff) << '\n'; //260
     return 0;
 }
-/*
- * 맨 마지막 열에서 가장 작은걸 가져가는 dp
- * 맨 마지막 열에 결과들을 저장해서 가장 작은것 출력
- * 22 23 17 19 21
- * 선택 가능한 영역은 본인이 (i, j)일때 (i-1, j-1), (i-1, j)
- */
