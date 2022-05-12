0e9698e63a61f60d72c1460ca94f98341b5f5653
YeongHoon Song <thddudgns972@gmail.com>
2022-05-12 13:59:07 +0900

    Create SolveTest.cpp

---
+++ 1Day1Baek/SolveTest.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <utility>
+#include <set>
+#include <cctype>
+using namespace std;
+void init() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main() {
+    init();
+    
+    return 0;
+}
+
+/*
+ */
